3fdfcacfd802f641997619d653798242871f04b6
JasonChien <s124202468@gmail.com>
2017-12-21 10:29:05 +0800

    Add expert setting activity

---
+++ app/src/main/java/com/chiachen/debugapppractices/ExpertSettingActivity.java
@@ -0,0 +1,24 @@
+package com.chiachen.debugapppractices;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jianjiacheng on 21/12/2017.
+ */
+
+public class ExpertSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_expert);
+
+        initUI();
+    }
+
+    private void initUI() {
+
+    }
+}
