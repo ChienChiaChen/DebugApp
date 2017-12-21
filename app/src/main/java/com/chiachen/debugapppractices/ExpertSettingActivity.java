99f9076b1fa41e432d399e27972dac2bd96b7ef2
JasonChien <s124202468@gmail.com>
2017-12-21 15:07:55 +0800

    Add basic info.

--- app/src/main/java/com/chiachen/debugapppractices/ExpertSettingActivity.java
+++ app/src/main/java/com/chiachen/debugapppractices/ExpertSettingActivity.java
@@ -3,6 +3,10 @@ package com.chiachen.debugapppractices;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+import com.chiachen.debugapppractices.utils.DeviceInfo;
+import com.chiachen.debugapppractices.utils.ui.PreferenceView;
 
 /**
  * Created by jianjiacheng on 21/12/2017.
@@ -19,6 +23,23 @@ public class ExpertSettingActivity extends AppCompatActivity {
     }
 
     private void initUI() {
+        {
+            LinearLayout list = findViewById(R.id.expert_mode_system_info_list);
 
+            list.addView(new PreferenceView.Builder(this)
+                    .setTitle(ExpertSettingHelper.ANDROID_OS)
+                    .setValue(DeviceInfo.getOSVersion())
+                    .build());
+
+            list.addView(new PreferenceView.Builder(this)
+                    .setTitle(ExpertSettingHelper.VENDOR)
+                    .setValue(DeviceInfo.getVendor())
+                    .build());
+
+            list.addView(new PreferenceView.Builder(this)
+                    .setTitle(ExpertSettingHelper.SYSTEM_COUNTRY)
+                    .setValue(DeviceInfo.getLocale())
+                    .build());
+        }
     }
 }
